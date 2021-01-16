bf4499aafa440e93d938b09329504717a05d5f5e
Dane Ericksen <10721110@my.uvu.edu>
2021-01-15 19:53:15 -0700

    Created first script
    Hello world

---
+++ BeginnersGame/Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    private int number;
+    private string myName;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = 1;
+        myName = "Dane";
+        
+        print("Hello " + myName + ", your number is " + number);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
