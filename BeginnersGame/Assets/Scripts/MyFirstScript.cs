d8ca33d1f9c1ee61a62e63cb5e2c500cd703d579
Dane Ericksen <10721110@my.uvu.edu>
2021-01-19 17:25:10 -0700

    Practiced making Methods

--- BeginnersGame/Assets/Scripts/MyFirstScript.cs
+++ BeginnersGame/Assets/Scripts/MyFirstScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,7 @@ public class MyFirstScript : MonoBehaviour
 {
     private int number;
     private string myName;
+    private int counter;
     
 
     // Start is called before the first frame update
@@ -13,13 +15,51 @@ public class MyFirstScript : MonoBehaviour
     {
         number = 1;
         myName = "Dane";
-        
         print("Hello " + myName + ", your number is " + number);
+
+        int localVariable = 3;
+        print("local variable: " + localVariable);
+
+        counter = 0;
+
+        myFirstMethod();
+        
+        mySecondMethod("Patrick");
+        myThirdMethod("Wilhelm", "Soelberg");
+
+        int myAge = calculateAge(1997);
+        print("Your age is " + myAge);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //print(myName);
+        //print("local variable: " +localVariable);
         
+        counter = counter + 1;
+        //print("counter = "+counter);
+    }
+
+    public void myFirstMethod()
+    {
+        print("Hello World");
+    }
+
+    public void mySecondMethod(string name)
+    {
+        print("Hello, your name is " +name);
+    }
+
+    public void myThirdMethod(string fName, string lName)
+    {
+        print("Hello, your name is " + fName + " " +lName);
+    }
+
+    public int calculateAge (int YOB)
+    {
+        int age;
+        age = 2021 - YOB;
+        return (age);
     }
 }
