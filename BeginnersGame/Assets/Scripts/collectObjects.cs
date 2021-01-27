5f203309401328b2a381c83723b475c04c6d445c
Dane Ericksen <10721110@my.uvu.edu>
2021-01-26 23:27:37 -0700

    Created Character Controller for Collecting Objects

---
+++ BeginnersGame/Assets/Scripts/collectObjects.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        print("Collision with object");
+    }
+}
